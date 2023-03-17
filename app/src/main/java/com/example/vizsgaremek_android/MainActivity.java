f1cd0abc7acec7c36e63a2641dd58c4bfc48ddb1
szoker <alex@gmail.com>
2023-03-17 15:00:25 +0100

    onFocus inputs

--- app/src/main/java/com/example/vizsgaremek_android/MainActivity.java
+++ app/src/main/java/com/example/vizsgaremek_android/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.vizsgaremek_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageLogo;
     private Animation fadeIn;
     private Button registerButton, logInButton, searchButton;
-    private TextView test;
+    private EditText editTextFirst, editTextSecond, editTextThird, editTextFourth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,37 @@ public class MainActivity extends AppCompatActivity {
         imageLogo.startAnimation(fadeIn);
 
         registerButton.setOnClickListener(v -> {
+            logInButton.setBackgroundColor(Color.TRANSPARENT);
+            logInButton.setTextColor(Color.BLACK);
+            registerButton.setBackgroundColor(Color.BLACK);
+            registerButton.setTextColor(Color.WHITE);
+            searchButton.setBackgroundColor(Color.TRANSPARENT);
+            searchButton.setTextColor(Color.BLACK);
+            editTextThird.setVisibility(View.VISIBLE);
+            editTextSecond.setVisibility(View.VISIBLE);
+            editTextFourth.setVisibility(View.VISIBLE);
+        });
 
+        logInButton.setOnClickListener(v -> {
+            registerButton.setBackgroundColor(Color.TRANSPARENT);
+            registerButton.setTextColor(Color.BLACK);
+            logInButton.setBackgroundColor(Color.BLACK);
+            logInButton.setTextColor(Color.WHITE);
+            searchButton.setBackgroundColor(Color.TRANSPARENT);
+            searchButton.setTextColor(Color.BLACK);
+            editTextThird.setVisibility(View.GONE);
+            editTextFourth.setVisibility(View.GONE);
+        });
+        searchButton.setOnClickListener(v -> {
+            registerButton.setBackgroundColor(Color.TRANSPARENT);
+            registerButton.setTextColor(Color.BLACK);
+            logInButton.setBackgroundColor(Color.TRANSPARENT);
+            logInButton.setTextColor(Color.BLACK);
+            searchButton.setBackgroundColor(Color.BLACK);
+            searchButton.setTextColor(Color.WHITE);
+            editTextThird.setVisibility(View.GONE);
+            editTextSecond.setVisibility(View.GONE);
+            editTextFourth.setVisibility(View.GONE);
         });
 
     }
@@ -43,6 +75,9 @@ public class MainActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.registerButton);
         logInButton = findViewById(R.id.logInButton);
         searchButton = findViewById(R.id.searchButton);
-        test = findViewById(R.id.test);
+        editTextFirst = findViewById(R.id.editTextFirst);
+        editTextSecond = findViewById(R.id.editTextSecond);
+        editTextThird = findViewById(R.id.editTextThird);
+        editTextFourth = findViewById(R.id.editTextFourth);
     }
 }
