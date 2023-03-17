15b6e6a4566ae9bdd1add7b2fe4b9976d2231ce7
szoker <alex@gmail.com>
2023-03-17 14:27:23 +0100

    basic page without functions

--- app/src/main/java/com/example/vizsgaremek_android/MainActivity.java
+++ app/src/main/java/com/example/vizsgaremek_android/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageLogo;
     private Animation fadeIn;
+    private Button registerButton, logInButton, searchButton;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,20 @@ public class MainActivity extends AppCompatActivity {
         videoview.start();
 
         imageLogo.startAnimation(fadeIn);
+
+        registerButton.setOnClickListener(v -> {
+
+        });
+
     }
+
 
     public void init(){
         imageLogo = findViewById(R.id.imageLogo);
         fadeIn = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        registerButton = findViewById(R.id.registerButton);
+        logInButton = findViewById(R.id.logInButton);
+        searchButton = findViewById(R.id.searchButton);
+        test = findViewById(R.id.test);
     }
 }
